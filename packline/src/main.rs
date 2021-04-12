d4e40969a5d1d855149177119983e02d6ef61b62
Vinícius Jabes <vinijabes@gmail.com>
2021-04-11 23:43:07 -0300

    feat(client): add consumer handler

--- packline/src/main.rs
+++ packline/src/main.rs
@@ -4,6 +4,7 @@ use packline_cli::client::connect;
 use packline_core::app::App;
 use packline_core::connector::{Connector, TCPConnector};
 use packline_flow::connector::FlowConnector;
+use std::thread::yield_now;
 
 #[tokio::main]
 async fn main() {
@@ -12,17 +13,26 @@ async fn main() {
 
         //TODO: detect program shutdown step and send oneshot signal.
         let (_tx, rx) = tokio::sync::oneshot::channel();
+        let (client_tx, client_rx) = tokio::sync::oneshot::channel();
 
         let mut connector = TCPConnector::new(Box::new(FlowConnector { app: &App {} }));
 
-        tokio::spawn(async {
-            let _ = connect("127.0.0.1:1883").await;
+        tokio::spawn(async move {
+            let mut client = connect("127.0.0.1:1883").await.unwrap();
+            client
+                .consume("testing_topic".to_string(), || {
+                    println!("Handling packets");
+                })
+                .await;
+
+            client_rx.await;
         });
 
         connector
             .run(app, tokio::runtime::Handle::current(), &mut rx.fuse())
             .await;
 
+        client_tx.send(true);
         println!("After run!")
     })
     .await;
--- packline_cli/src/client.rs
+++ packline_cli/src/client.rs
@@ -1,10 +1,20 @@
-use crate::connection::Connection;
+use tokio::net::{TcpStream, ToSocketAddrs};
+
 use packline_flow::messages::connect::ConnectRequestV1;
 use packline_flow::messages::Message;
-use tokio::net::{TcpStream, ToSocketAddrs};
+
+use crate::connection::Connection;
+
+use tokio::runtime::Handle;
+use tokio::sync::mpsc::channel;
+use tokio::sync::mpsc::Receiver;
+use tokio::sync::mpsc::Sender;
 
 pub struct Client {
     connection: Connection,
+
+    consumer_worker: Sender<(String, Box<Fn() -> () + Send>)>,
+    consumers: Vec<Sender<bool>>,
 }
 
 pub async fn connect<T: ToSocketAddrs>(addr: T) -> Result<Client, Box<dyn std::error::Error>> {
@@ -16,5 +26,37 @@ pub async fn connect<T: ToSocketAddrs>(addr: T) -> Result<Client, Box<dyn std::e
         .send((1, 1), Message::ConnectRequestV1(ConnectRequestV1 {}))
         .await?;
 
-    Ok(Client { connection })
+    let (tx, mut rx) = channel(16);
+
+    Handle::current().spawn(Client::consumer_worker(rx));
+
+    Ok(Client {
+        connection,
+        consumer_worker: tx,
+        consumers: Vec::new(),
+    })
+}
+
+impl Client {
+    pub async fn consume<F>(&mut self, topic: String, handler: F)
+    where
+        F: Fn() -> () + Send + 'static,
+    {
+        self.consumer_worker.send((topic, Box::new(handler))).await;
+    }
+
+    async fn consumer_worker<F>(mut rx: Receiver<(String, F)>)
+    where
+        F: Fn() -> (),
+    {
+        println!("Starting consumer worker {:?}", rx);
+
+        while let Some((topic, handler)) = rx.recv().await {
+            println!("{}", topic);
+
+            //TODO: start a stream between client and server.
+        }
+
+        println!("Stopping consumer worker {:?}", rx);
+    }
 }
